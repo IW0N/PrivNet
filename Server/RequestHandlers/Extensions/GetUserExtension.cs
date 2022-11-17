0787122d4fdcbcba5590094aa10f21d2a1f27d95
IW0N <world9655@gmail.com>
2022-11-17 10:07:12 +0300

    added 'clicked' event to UserBox

---
+++ Server/RequestHandlers/Extensions/GetUserExtension.cs
@@ -0,0 +1,20 @@
+﻿using Common.Responses;
+using Server.Database.Base;
+
+namespace Server.RequestHandlers.Extensions
+{
+    internal static class GetUserExtension
+    {
+        public static GetUserResponse BuildByUser(User user)
+        {
+            return user is null ?
+                new GetUserResponse { Exists = false } :
+                new GetUserResponse
+                {
+                    Avatar = null,
+                    Nickname = user.Name,
+                    Id = user.Id
+                };
+        }
+    }
+}
---
+++ Server/Services/NoRequiredRegistPathes.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Server.Services
+{
+    public class NoRequiredRegisterPaths
+    {
+        public Dictionary<string, string[]> Paths { get; set; }
+        public static bool PathNotRequireRegister(HttpContext context)
+        {
+            
+            var services = context.RequestServices;
+            var config = services.GetService<IConfiguration>();
+            var conf = config.Get<NoRequiredRegisterPaths>();
+            string path = context.Request.Path;
+            string method = context.Request.Method;
+            var paths = conf.Paths;
+            bool result=!(paths.ContainsKey(path) && paths[path].Contains(method));
+            return result;
+        }
+    }
+}
